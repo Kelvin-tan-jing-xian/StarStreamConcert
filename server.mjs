0bb1165371d92dd8c65052980460adfc835b1096
Kelvin-tan-jing-xian <kelvintanjingxian@gmail.com>
2021-05-21 19:03:43 +0800

    changed venueTime and venuePrice to be radio buttons

--- server.mjs
+++ server.mjs
@@ -1,22 +1,20 @@
+import Express from "express";
+import ExpHandlebars from "express-handlebars";
+import ExpSession from "express-session";
+import BodyParser from "body-parser";
+import CookieParser from "cookie-parser";
 
+import MethodOverrides from "method-override";
+import Path from "path";
 
-import Express         from 'express';
-import ExpHandlebars   from 'express-handlebars';
-import ExpSession      from 'express-session';
-import BodyParser      from 'body-parser';
-import CookieParser    from 'cookie-parser';
+import Flash from "connect-flash";
+import FlashMessenger from "flash-messenger";
 
-import MethodOverrides from 'method-override';
-import Path            from 'path';
-
-import Flash           from 'connect-flash';
-import FlashMessenger  from 'flash-messenger';
-
-import Handlebars      from 'handlebars';
-import { allowInsecurePrototypeAccess } from '@handlebars/allow-prototype-access';
+import Handlebars from "handlebars";
+import { allowInsecurePrototypeAccess } from "@handlebars/allow-prototype-access";
 
 const Server = Express();
-const Port   = process.env.PORT || 3000;
+const Port = process.env.PORT || 3000;
 
 /**
  * Template Engine
@@ -25,38 +23,42 @@ const Port   = process.env.PORT || 3000;
  * Trust me it saves your time more.
  * https://www.npmjs.com/package/express-nunjucks
  */
-Server.set('views',       'templates');		//	Let express know where to find HTML templates
-Server.set('view engine', 'handlebars');	//	Let express know what template engine to use
-Server.engine('handlebars', ExpHandlebars({
-	handlebars:     allowInsecurePrototypeAccess(Handlebars),
-	defaultLayout: 'main'
-}));
+Server.set("views", "templates"); //	Let express know where to find HTML templates
+Server.set("view engine", "handlebars"); //	Let express know what template engine to use
+Server.engine(
+  "handlebars",
+  ExpHandlebars({
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+    defaultLayout: "main",
+  })
+);
 //	Let express know where to access static files
 //	Host them at locahost/public
-Server.use("/public", Express.static('public'));
+Server.use("/public", Express.static("public"));
 
 /**
  * Form body parsers etc
  */
-Server.use(BodyParser.urlencoded( { extended: false }));
+Server.use(BodyParser.urlencoded({ extended: false }));
 Server.use(BodyParser.json());
 Server.use(CookieParser());
-Server.use(MethodOverrides('_method'));
+Server.use(MethodOverrides("_method"));
 
-
-import { SessionStore, initialize_database } from './data/database.mjs'
+import { SessionStore, initialize_database } from "./data/database.mjs";
 /**
  * Express Session
  */
- Server.use(ExpSession({
-	name:   'example-app',
-	secret: 'random-secret',
-	store:   SessionStore,
-	resave:  false,
-	saveUninitialized: false
-}));
+Server.use(
+  ExpSession({
+    name: "example-app",
+    secret: "random-secret",
+    store: SessionStore,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-import { initialize_passport } from './utils/passport.mjs';
+import { initialize_passport } from "./utils/passport.mjs";
 
 /**
  * Initialize passport
@@ -69,7 +71,7 @@ initialize_passport(Server);
 initialize_database(false);
 
 /**
- * Flash Messenger 
+ * Flash Messenger
  */
 Server.use(Flash());
 Server.use(FlashMessenger.middleware);
@@ -82,29 +84,28 @@ Server.use(FlashMessenger.middleware);
  * TODO: Setup global contexts here. Basically stuff your variables in locals
  */
 Server.use(function (req, res, next) {
-	res.locals.user = req.user || null;
-	next();
+  res.locals.user = req.user || null;
+  next();
 });
 
-
-import Routes from './routes/main.mjs'
+import Routes from "./routes/main.mjs";
 Server.use("/", Routes);
 
 /**
  * DEBUG USAGE
  * Use this to check your routes
  * Prints all the routes registered into the application
-**/
-import { ListRoutes } from './utils/routes.mjs'
+ **/
+import { ListRoutes } from "./utils/routes.mjs";
 console.log(`=====Registered Routes=====`);
-ListRoutes(Server._router).forEach(route => {
-	console.log(`${route.method.padStart(8)} | /${route.path}`);
+ListRoutes(Server._router).forEach((route) => {
+  console.log(`${route.method.padStart(8)} | /${route.path}`);
 });
 console.log(`===========================`);
 
 /**
  * Start the server in infinite loop
  */
-Server.listen(Port, function() {
-	console.log(`Server listening at port ${Port}`);
+Server.listen(Port, function () {
+  console.log(`Server listening at port ${Port}`);
 });
